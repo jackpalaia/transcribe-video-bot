08f41f8bfe52791cf718c31d615009f6a156d871
Jack Palaia <jack@jackpalaia.com>
2021-08-07 03:00:00 -0400

    implement commenting on signal comments

--- main.py
+++ main.py
@@ -5,14 +5,40 @@ from dotenv import load_dotenv
 
 load_dotenv()
 
-reddit = praw.Reddit(
-    user_agent="transcribe-video-bot (by u/transcribe-video-bot)",
-    client_id=os.getenv("CLIENT_ID"),
-    client_secret=os.getenv("CLIENT_SECRET"),
-    username="transcribe-video-bot",
-    password=os.getenv("PASSWORD"),
-)
+signals = ["transcribe", "transcription"]
+validUrls = ["youtube", "youtu.be"]
 
-subreddit = reddit.subreddit("Videos")
-for submission in subreddit.stream.submissions():
-    print(submission)
+
+def main():
+    reddit = praw.Reddit(
+        user_agent="transcribe-video-bot (by u/transcribe-video-bot)",
+        client_id=os.getenv("CLIENT_ID"),
+        client_secret=os.getenv("CLIENT_SECRET"),
+        username="transcribe-video-bot",
+        password=os.getenv("PASSWORD"),
+    )
+
+    subreddit = reddit.subreddit("test")
+    for comment in subreddit.stream.comments():
+        if isCommentValid(comment):
+            print(comment.permalink)
+            comment.reply("hello!")
+
+
+# Returns true if the comment is a valid signal.
+# Checks if the comment contains a signal string,
+# if the comment is on a post that is a video,
+# and if the comment was not made by a bot.
+def isCommentValid(comment):
+    return (
+        any(s in comment.body for s in signals)
+        and (
+            comment.submission.is_video
+            or any(url in comment.submission.url for url in validUrls)
+        )
+        and "bot" not in comment.body
+    )
+
+
+if __name__ == "__main__":
+    main()
