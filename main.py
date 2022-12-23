5a1a6a498639ee3a82bf633a2f92cae640ea8ae9
jackpalaia <jack@jackpalaia.com>
2022-12-23 17:48:11 -0500

    change summoning to username, fix comment formatting

--- main.py
+++ main.py
@@ -6,9 +6,6 @@ from youtube_transcript_api import YouTubeTranscriptApi
 
 load_dotenv()
 
-signals = ["transcribe", "transcription"]
-validUrls = ["youtube", "youtu.be"]
-
 
 def main():
     reddit = praw.Reddit(
@@ -20,42 +17,32 @@ def main():
     )
 
     subreddit = reddit.subreddit("test")
-    for comment in subreddit.stream.comments():
+    for comment in subreddit.stream.comments(skip_existing=True):
         if isCommentValid(comment):
             url = comment.submission.url
             video_id = url.split("=")[1]
             transcript_list = []
             try:
-                transcript_list = YouTubeTranscriptApi.get_transcript(video_id=video_id)
+                transcript_list = YouTubeTranscriptApi.get_transcript(
+                    video_id=video_id)
             except:
                 continue
-            
-            transcript = "Here is your transcription:\n\n"
+
+            transcript = "Here is your transcription:\n\n>"
             for part in transcript_list:
-                text = part['text'].replace("\n", " ").replace(" !", "!").replace(" ?", "?") + " "
+                text = part['text'].replace("\n", " ").replace(
+                    " !", "!").replace(" ?", "?") + " "
                 transcript += text
-            transcript += "\n\nThis action was performed by a bot. Please contact u/MangoToothpaste if there are any issues."
-            
+            transcript += "\n\n*This action was performed by a bot. Please contact u/MangoToothpaste if there are any issues.*"
+
             comment.reply(transcript)
-                    
-            # Reply with the video transcription
 
 
-
-# Returns true if the comment is a valid signal.
-# Checks if the comment contains a signal string,
-# if the comment is on a post that is a video,
-# and if the comment was not made by a bot.
 def isCommentValid(comment):
-    return (
-        any(s in comment.body for s in signals)
-        and (
-            comment.submission.is_video
-            or any(url in comment.submission.url for url in validUrls)
-        )
-        and "bot" not in comment.body
-        and comment.submission.author is not None
-    )
+    """
+    Returns true if the comment contains content that summons the bot.
+    """
+    return comment.body == "u/transcribe-video-bot"
 
 
 if __name__ == "__main__":
