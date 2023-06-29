939d3b1e81e6e9320bdd2be93ef0c555186d5dc4
jackpalaia <jack@jackpalaia.com>
2023-06-29 01:22:01 -0400

    update bot

--- README.md
+++ README.md
@@ -1,5 +1,5 @@
 Reddit bot that transcribes video dialog.
 
-Currently running on an Google Cloud Compute Engine instance.
+Currently running on an AWS EC2 instance.
 
 Call by commenting "u/transcribe-video-bot" on the post with the video you'd like to transcribe.
--- main.py
+++ main.py
@@ -1,16 +1,14 @@
 import os
-import praw
+from datetime import datetime
 
+import praw
 from dotenv import load_dotenv
 from youtube_transcript_api import YouTubeTranscriptApi
-from datetime import datetime
 
 load_dotenv()
 
 
 def main():
-    print(datetime.now())
-
     reddit = praw.Reddit(
         user_agent="transcribe-video-bot (by u/transcribe-video-bot)",
         client_id=os.getenv("CLIENT_ID"),
@@ -19,13 +17,16 @@ def main():
         password=os.getenv("PASSWORD"),
     )
 
-    unread_messages = []
-    for item in reddit.inbox.unread(limit=None):
-        print(item.body)
-        if isinstance(item, praw.models.Comment) and isCommentSummon(item):
-            unread_messages.append(item)
+    print("Client started and Reddit instance created, accepting mentions")
+
+    for item in reddit.inbox.stream():
+        if "u/transcribe-video-bot" in item.body:
+            log_file = open("log.txt", "a")
+
+            log_file.write(f"{datetime.now()}\n")
+            log_file.write(
+                f"Mention from user {item.author.name} with id {item.id} at link {item.context}\n")
             url = item.submission.url
-            print(url)
             video_id = url[-11:]
             transcript_list = []
             try:
@@ -35,6 +36,7 @@ def main():
                 continue
 
             transcript = "Here is your transcription:\n\n>"
+            text = ""
             for part in transcript_list:
                 text = part['text'].replace("\n", " ").replace(
                     " !", "!").replace(" ?", "?") + " "
@@ -42,15 +44,12 @@ def main():
             transcript += "\n\n*This action was performed by a bot. Please contact u/MangoToothpaste if there are any issues.*"
 
             item.reply(transcript)
+            log_file.write(
+                f"Mention with id {item.id} replied to with transcript '{' '.join(text.split()[:10])}...'\n")
+            item.mark_read()
 
-    reddit.inbox.mark_read(unread_messages)
-
-
-def isCommentSummon(comment):
-    """
-    Returns true if the comment contains content that summons the bot.
-    """
-    return comment.body == "u/transcribe-video-bot"
+            log_file.write("\n")
+            log_file.close()
 
 
 if __name__ == "__main__":
